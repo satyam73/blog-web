67c64fb121996acf2a94c702216767160f3e570b
Satyam <bajpai1973satyam@gmail.com>
2023-09-25 21:30:50 +0530

    styles: add more stylings to navbar

--- components/common/Navbar/Navbar.js
+++ components/common/Navbar/Navbar.js
@@ -1,8 +1,14 @@
 import { Box } from "@mui/material";
 import NavbarPresentation from "./NavbarPresentation";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [activeLinkIndex, setActiveLinkIndex] = useState(0);
+
+  function onNavbarItemClick(index) {
+    setActiveLinkIndex(index)
+  }
   return (
-    <NavbarPresentation />
+    <NavbarPresentation activeLinkIndex={activeLinkIndex} onNavbarItemClick={onNavbarItemClick} />
   )
 }
--- components/common/Navbar/NavbarPresentation.js
+++ components/common/Navbar/NavbarPresentation.js
@@ -2,10 +2,11 @@ import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/mater
 import styles from './navbar.module.css';
 import { NAVBAR_ITEMS } from "./navbar.constant";
 import { useTheme } from "@mui/material";
-export default function NavbarPresentation() {
+
+export default function NavbarPresentation({ activeLinkIndex, onNavbarItemClick }) {
   const theme = useTheme();
-  const navbarItemsMapping = NAVBAR_ITEMS.map((item) => (
-    <Button data-testid={`navbar-item-${item.name}`} key={item.name} sx={{ color: theme.palette.primary.dark }}>
+  const navbarItemsMapping = NAVBAR_ITEMS.map((item, index) => (
+    <Button onClick={() => onNavbarItemClick(index)} className={`${styles['navbar__item']} ${activeLinkIndex == index ? styles['navbar__item--active'] : ''}`} data-testid={`navbar-item-${item.name}`} key={item.name} sx={{ color: theme.palette.primary.dark }}>
       {item.text}
     </Button>
   ));
@@ -14,9 +15,10 @@ export default function NavbarPresentation() {
     <AppBar component="nav">
       <Toolbar>
         <Typography
-          variant="h6"
+          variant="h5"
           component="div"
           sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          className={styles['navbar__logo']}
         >
           Bloggie
         </Typography>
