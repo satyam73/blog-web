a90d1f5ef633d252b2734406f9e9021f8e05e247
Satyam <bajpai1973satyam@gmail.com>
2023-10-08 21:19:47 +0530

    feat: add page navigation in navbar

--- components/common/Navbar/Navbar.js
+++ components/common/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import { Box } from "@mui/material";
 import NavbarPresentation from "./NavbarPresentation";
 import { useState } from "react";
+import { useRouter } from "next/router";
+import { NAVBAR_ITEMS } from "./navbar.constant";
 
 export default function Navbar() {
   const [activeLinkIndex, setActiveLinkIndex] = useState(0);
-
+  const { push } = useRouter();
   function onNavbarItemClick(index) {
-    setActiveLinkIndex(index)
+    setActiveLinkIndex(index);
+    const activePage = NAVBAR_ITEMS[index].link;
+    push(activePage);
   }
   return (
     <NavbarPresentation activeLinkIndex={activeLinkIndex} onNavbarItemClick={onNavbarItemClick} />
