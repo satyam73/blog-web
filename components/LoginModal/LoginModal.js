8542ccf3efa1f6eccdff390b565edda90964a9eb
Satyam <bajpai1973satyam@gmail.com>
2023-10-24 01:28:54 +0530

    refactor: login modal code

--- components/LoginModal/LoginModal.js
+++ components/LoginModal/LoginModal.js
@@ -1,12 +1,7 @@
-import { Box, Button, IconButton } from '@mui/material';
-import Modal from '../common/Modal/Modal';
-import Image from 'next/image';
-import InputBox from '../common/Input/InputBox';
-import CloseIcon from '@mui/icons-material/Close';
-import styles from './loginModal.module.css';
 import { useState } from 'react';
 import { emailValidator, passwordValidator } from '@/utilities/validators';
 import signIn from '@/firebase/auth/signin';
+import LoginModalPresentation from './LoginModalPresentation';
 
 export default function LoginModal({ open, handleClose, }) {
   const [loginDetails, setLoginDetails] = useState({ email: '', password: '' });
@@ -51,52 +46,13 @@ export default function LoginModal({ open, handleClose, }) {
     }
   }
   return (
-    <Modal open={open} onClose={handleClose}>
-      <Box className={styles['login-modal']}>
-        <IconButton
-          className={styles['login-modal__close-button']}
-          onClick={handleClose}
-        >
-          <CloseIcon fontSize='medium' />
-        </IconButton>
-        <Box
-          component='span'
-          className={styles['login-modal__image-container']}
-        >
-          <Image
-            className={styles['image-container__image']}
-            src={'assets/login.svg'}
-            alt='welcome cat illustration'
-            height={130}
-            width={130}
-          />
-        </Box>
-        <Box className={styles['login-modal__main']}>
-          <form className={styles['main__form']}>
-            <InputBox
-              value={loginDetails.email}
-              type={'text'}
-              name={'Email'}
-              placeholder={'Enter your email'}
-              id='email'
-              handleChange={handleChange}
-              isValid={isDetailsValid.email}
-            />
-            <InputBox
-              value={loginDetails.password}
-              type={'password'}
-              name={'Password'}
-              placeholder={'Enter your password'}
-              id='password'
-              handleChange={handleChange}
-              isValid={isDetailsValid.password}
-            />
-            <Button onClick={handleLogin} variant='contained' className={styles['form__button']}>
-              Login
-            </Button>
-          </form>
-        </Box>
-      </Box>
-    </Modal>
-  );
+    <LoginModalPresentation
+      open={open}
+      handleClose={handleClose}
+      handleChange={handleChange}
+      handleLogin={handleLogin}
+      isDetailsValid={isDetailsValid}
+      loginDetails={loginDetails}
+    />
+  )
 }
