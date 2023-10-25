06e1eef43a96c9c74b89c3792c2fcaf0df152e28
Satyam <bajpai1973satyam@gmail.com>
2023-10-25 19:11:51 +0530

    feat: add user context for logged in user

---
+++ Contexts/UserProvider.js
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { createContext } from "react";
+import {
+  onAuthStateChanged,
+  getAuth,
+} from 'firebase/auth';
+import firebaseApp from '@/firebase/config';
+
+const auth = getAuth(firebaseApp);
+export const UserContext = createContext({});
+export const useUser = () => useContext(UserContext);
+
+export default function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, loading }}>
+      {children}
+    </UserContext.Provider >
+  )
+}
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,24 @@
 import { Box } from "@mui/material";
+import UserProvider, { useUser } from "@/Contexts/UserProvider";
+import Layout from "@/components/common/Layout/Layout";
 
 export default function Profile() {
+  const { user, loading } = useUser();
+  console.log(user);
+
   return <Box>
-    Profile page
-  </Box>
+    {loading ? <>loading...</> :
+      user?.displayName
+    }
+  </Box >
+}
+
+Profile.getLayout = function getLayout(page) {
+  return (
+    <UserProvider>
+      <Layout>
+        {page}
+      </Layout>
+    </UserProvider>
+  )
 }
