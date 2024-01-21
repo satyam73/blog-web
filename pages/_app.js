8a04fc25e870f564ceddd8ce149d91e3518d51aa
Satyam <bajpai1973satyam@gmail.com>
2024-01-21 16:09:55 +0530

    chore: remove folder contexts

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { Montserrat } from 'next/font/google';
 import ToastProvider from '@/app/contexts/ToastProvider';
 import Loader from '@/app/components/Loader/Loader';
 import { store } from '@/app/store/store'
+import UserProvider from '@/app/contexts/UserProvider';
 import '../styles/globals.css';
 const montserrat = Montserrat({ subsets: ['latin'] });
 export default function MyApp({ Component, pageProps }) {
