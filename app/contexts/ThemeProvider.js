492a134f42b2aa05514ef024f68326fe0b89b5ae
Satyam <bajpai1973satyam@gmail.com>
2024-01-28 21:40:16 +0530

    styles: fix style of bottom navbar

--- app/contexts/ThemeProvider.js
+++ app/contexts/ThemeProvider.js
@@ -116,7 +116,7 @@ theme = createTheme(theme, {
         root: {
           backgroundColor: theme.palette.primary.light,
           color: theme.palette.primary.dark,
-          position: 'sticky',
+          position: 'fixed',
           top: 'auto',
           bottom: '0',
           bottom: '0px',
